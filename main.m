2ed9f407fd15f76f0124135922e77905f6935e38
mirmohammad <mir.mohammad.saadati@gmail.com>
2019-08-30 12:40:19 -0400

    replaced for loop with while loop

--- main.m
+++ main.m
@@ -10,8 +10,9 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        for(int i = 0; i < 10; i++) {
-            printf("%d\n", i);
+        int i = 0;
+        while(i < 10) {
+            printf("%d\n", i++);
         }
     }
     return 0;
