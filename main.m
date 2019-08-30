8b37b3af4f73c881dfe857c20edb7ee3d5938cc7
mirmohammad <mir.mohammad.saadati@gmail.com>
2019-08-30 12:37:03 -0400

    Added two files for test

---
+++ main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  demo
+//
+//  Created by Mirmohammad Saadati on 2019-08-30.
+//  Copyright © 2019 Mirmohammad Saadati. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        for(int i = 0; i < 10; i++) {
+            printf("%d\n", i);
+        }
+    }
+    return 0;
+}
